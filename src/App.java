13e4c34b9965a23b6ba04b2a083d612b0c746e81
Pritam Kumar Biswas <126695473+PritamBiswas06@users.noreply.github.com>
2023-08-02 04:03:07 +0000

    Work with data inside arrays

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
 public class App {
   
   public static void main(String[] args) {
-    
+    double[] number={10,20,30,40,50};
+    System.out.println(number[2]);
+    number[2]=60;
+    System.out.println(number[2]);
+
   }
 }
